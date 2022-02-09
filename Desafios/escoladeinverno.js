47fc092ff31f4cb53df60bf902a9ba6d8ec00203
Paulo-gsilva <guigui041201@gmail.com>
2022-02-09 16:49:57 -0300

    desafios JavaScript

---
+++ Desafios/escoladeinverno.js
@@ -0,0 +1,15 @@
+let lines = gets().split("\n");
+let n = parseInt(lines.shift());
+
+
+if (n >= 1 && n <= 34) {
+
+const text = "Life is not a problem to be solved, but a reality to be experienced.";
+
+const letter = text.substr(0, n)
+
+const uperCase = letter.toUpperCase();
+
+print(uperCase);
+
+}
