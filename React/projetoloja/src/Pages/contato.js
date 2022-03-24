820bcd5590679f7c93ffcd6745b457183e337d51
Paulo-gsilva <guigui041201@gmail.com>
2022-03-24 10:05:38 -0300

    adicionando pagina contato

--- React/projetoloja/src/Pages/contato.js
+++ React/projetoloja/src/Pages/contato.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Grid, Button, TextField } from '@material-ui/core/';
 
 export default function Contatos() {
     return (
-        <h1>Contatos</h1>
+        <>
+            <Grid container direction="row" xs={12}>
+                <TextField id="name" label="Name" fullWidth />
+                <TextField id="message" label="Message" fullWidth />
+            </Grid>
+            <Button variant="contained" color="primary">
+                Enviar
+            </Button>
+        </>
     );
 }
--- React/projetoloja/src/Pages/home.js
+++ React/projetoloja/src/Pages/home.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Grid, Button, Container, Typography, Paper, List, ListItem, ListItemText } from '@material-ui/core/';
 
 export default function Home() {
     return (
-        <h1>Home</h1>
+        
     );
 }
