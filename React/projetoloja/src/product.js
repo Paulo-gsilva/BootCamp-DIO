30233ba2aad13a0ebfd3f61b8a267476208315d5
Paulo-gsilva <guigui041201@gmail.com>
2022-03-25 15:38:26 -0300

    criando arquivo com dados do banco de dados

---
+++ React/projetoloja/src/product.js
@@ -0,0 +1,90 @@
+const productList = [
+    {
+        id_product: 1,
+        category: "nacional",
+        fk_idcategorys: 1,
+        name_product: "Flamengo 2020",
+        price: 28.90,
+        image: "../../public/images/super.jpg",
+        id_category: 1,
+        name_category: "Clubes Brasileiros",
+        selected: false
+    },
+    {
+        id_product: 2,
+        category: "nacional",
+        fk_idcategorys: 1,
+        name_product: "Ponte Preta 2020",
+        price: 28.90,
+        image: "../../public/images/super.jpg",
+        id_category: 1,
+        name_category: "Clubes Brasileiros",
+        selected: false
+    },
+    {
+        id_product: 3,
+        category: "nacional",
+        fk_idcategorys: 1,
+        name_product: "Corinthians 2020",
+        price: 28.90,
+        image: "../../public/images/super.jpg",
+        id_category: 1,
+        name_category: "Clubes Brasileiros",
+        selected: false
+    },
+    {
+        id_product: 4,
+        category: "nacional",
+        fk_idcategorys: 1,
+        name_product: "Ceará 2020",
+        price: 28.90,
+        image: "../../public/images/super.jpg",
+        id_category: 1,
+        name_category: "Clubes Brasileiros",
+        selected: false
+    },
+    {
+        id_product: 5,
+        category: "nacional",
+        fk_idcategorys: 1,
+        name_product: "São Paulo 2020",
+        price: 28.90,
+        image: "../../public/images/super.jpg",
+        id_category: 1,
+        name_category: "Clubes Brasileiros",
+        selected: false
+    },
+    {
+        id_product: 6,
+        category: "nacional",
+        fk_idcategorys: 1,
+        name_product: "Cruzeiro 2020",
+        price: 28.90,
+        image: "../../public/images/super.jpg",
+        id_category: 1,
+        name_category: "Clubes Brasileiros",
+        selected: false
+    },
+    {
+        id_product: 7,
+        category: "nacional",
+        fk_idcategorys: 1,
+        name_product: "Internacional 2020",
+        price: 28.90,
+        image: "../../public/images/super.jpg",
+        id_category: 1,
+        name_category: "Clubes Brasileiros",
+        selected: false
+    },
+    {
+        id_product: 8,
+        category: "nacional",
+        fk_idcategorys: 1,
+        name_product: "Grêmio 2020",
+        price: 28.90,
+        image: "../../public/images/super.jpg",
+        id_category: 1,
+        name_category: "Clubes Brasileiros",
+        selected: false
+    },
+]
