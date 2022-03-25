1919a4d31dd4089b9c229a7c49d7a8fc5858dc5a
Paulo-gsilva <guigui041201@gmail.com>
2022-03-25 14:47:46 -0300

    adicionando CONTAINER no APP para que todos os componentes recebam seu espaçamento

--- React/projetoloja/src/App.js
+++ React/projetoloja/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MainRoutes from "./routes";
+import { Container } from '@material-ui/core/';
 import Header from './Components/Header';
 
 export default function App() {
   //forçando a renderização do header com <Header/>
   return (
-    <BrowserRouter>
-      <Header />
-      <MainRoutes />
-    </BrowserRouter>
-
+    <Container maxWidth="xl">
+      <BrowserRouter>
+        <Header />
+        <MainRoutes />
+      </BrowserRouter>
+    </Container>
   );
 }
 
